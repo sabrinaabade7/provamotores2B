ed7ac4d30bc956e4bfaa140ba68fe0e48d539367
sabrinaabade7 <sabrina.abade@escolar.ifrn.edu.br>
2025-09-23 10:31:43 -0300

    2309

--- provamotores2/Assets/Script/MovePig.cs
+++ provamotores2/Assets/Script/MovePig.cs
@@ -6,16 +6,17 @@ public class Move : MonoBehaviour
 {
     public Transform target;
     public SpriteRenderer sr;
-    
+
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, 10 * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, target.position, 5 * Time.deltaTime);
         
         if (transform.position.x > target.position.x)
             sr.flipX = false;
